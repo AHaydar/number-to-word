6dd7a35cc0dc130103dafcbb283a56735525e23c
Ali Haydar <a.haydar85@gmail.com>
2018-08-17 22:52:36 +1200

    Add a new extension to the dockerfile to allow instantiating PHP NumberFormatter class. Transform the input to word, in the same php file

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
 FROM php:7-fpm
 RUN docker-php-ext-install mysqli pdo pdo_mysql
+RUN apt-get update && \
+    apt-get install -y libicu-dev && \
+    docker-php-ext-install intl
--- index.php
+++ index.php
@@ -1,8 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: ahaydar
- * Date: 8/14/18
- * Time: 6:05 AM
- */
-echo phpinfo();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $f = new NumberFormatter("en", NumberFormatter::SPELLOUT);
+    $word = $f->format($_POST["number"]);
+}
+?>
+    <header>
+        <h1>
+            Welcome!! Would you like to convert a number to a word?
+        </h1>
+    </header>
+    <div>
+        <form method="post" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+            Number: <input name="number">
+            <br>
+            <br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+    <div>
+        <br>
+
+        Your word is: <?= $word ?>
+    </div>
+
+</body>
+</html>
